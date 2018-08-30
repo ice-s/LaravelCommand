e46372a925aaf52b3401eb6596bd368463c6b70c
nguy_n_tr_ng_s_n <son.nguyen.truong@ntq-solution.com.vn>
2018-08-30 17:55:06 +0700

    'add'

--- app/Console/Commands/ServiceMakeCommand.php
+++ app/Console/Commands/ServiceMakeCommand.php
@@ -30,56 +30,157 @@ class ServiceMakeCommand extends Command
         $names = $this->argument('name');
 
         foreach ($names as $name) {
-            $this->makeService($name);
+            $this->make($name);
         }
     }
 
     protected function isExistBaseService($path = '')
     {
-        $path = app_path('/Http/Services');
         $baseFile = 'BaseService.php';
-        if (!file_exists($path = app_path($path . "/" . $baseFile))) {
-            mkdir($path, 0777, true);
+        $path = app_path('/Services');
 
-            return true;
+        if (!file_exists(app_path("/Services/{$baseFile}"))) {
+            $this->makeDir($path);
+
+            $template = file_get_contents(resource_path("stubs/Base/BaseService.stub"));
+            file_put_contents($path . "/{$baseFile}", $template);
+
+            return false;
         }
 
-        return false;
+        return true;
     }
 
     protected function isExistBaseRepository($path = '')
     {
+        $baseFile = 'BaseRepository.php';
+        $path = app_path('/Entities/Repositories');
 
+        if (!file_exists(app_path("/Entities/Repositories/{$baseFile}"))) {
+            $this->makeDir($path);
+
+            $template = file_get_contents(resource_path("stubs/Base/BaseRepository.stub"));
+            file_put_contents($path . "/{$baseFile}", $template);
+
+            return false;
+        }
+
+        return true;
     }
 
     protected function isExistBaseModel($path = '')
     {
+        $baseFile = 'BaseModel.php';
+        $path = app_path('/Entities/Models');
 
+        if (!file_exists(app_path("/Entities/Models/{$baseFile}"))) {
+            $this->makeDir($path);
+
+            $template = file_get_contents(resource_path("stubs/Base/BaseModel.stub"));
+            file_put_contents($path . "/{$baseFile}", $template);
+
+            return false;
+        }
+
+        return true;
     }
 
-    protected function makeService($name)
+    protected function make($name)
     {
-        if(!$this->isExistBaseService()){
+        if (!$this->isExistBaseService()) {
             /*create base Service*/
-            echo "BaseService not exist";
+            echo "Create new Base Service\n";
         }
         /*Create service*/
+        $this->makeService($name);
 
+        if (!$this->isExistBaseModel()) {
+            /*create base Service*/
+            echo "Create new Base Model\n";
+        }
         /*Create Model*/
         $this->makeModel($name);
 
+        if (!$this->isExistBaseRepository()) {
+            /*create base Service*/
+            echo "Create new Base Repository\n";
+        }
         /*Create Repository*/
         $this->makeRepository($name);
     }
 
-    protected function makeRepository($name)
+    protected function makeService($name)
     {
 
+        $serviceFolder = $name;
+        $serviceClass = "{$name}Service.php";
+        $folderPath = app_path("/Services/{$serviceFolder}");
+        $fileName = app_path("/Services/{$serviceFolder}/{$serviceClass}");
+
+        if (!file_exists($fileName)) {
+            $this->makeDir($folderPath);
+
+            $serviceTemplate = file_get_contents(resource_path("stubs/Service.stub"));
+            $serviceTemplate = str_replace(['{{Name}}'], [$name], $serviceTemplate);
+            file_put_contents($fileName, $serviceTemplate);
+
+            return true;
+        } else {
+            echo "WARNING : Services is existed\n";
+
+            return false;
+        }
+    }
+
+    protected function makeRepository($name)
+    {
+        $repoFolder = $name;
+        $repoClass = "{$name}Repository.php";
+        $folderPath = app_path("/Entities/Repositories/{$repoFolder}");
+        $fileName = app_path("/Entities/Repositories/{$repoFolder}/{$repoClass}");
+
+        if (!file_exists($fileName)) {
+            $this->makeDir($folderPath);
+
+            $serviceTemplate = file_get_contents(resource_path("stubs/Repository.stub"));
+            $serviceTemplate = str_replace(['{{Name}}'], [$name], $serviceTemplate);
+            file_put_contents($fileName, $serviceTemplate);
+
+            return true;
+        } else {
+            echo "WARNING : Services is existed\n";
+
+            return false;
+        }
     }
 
     protected function makeModel($name)
     {
+        $modelFolder = $name;
+        $modelClass = "{$name}Model.php";
+        $folderPath = app_path("/Entities/Models/{$modelFolder}");
+        $fileName = app_path("/Entities/Models/{$modelFolder}/{$modelClass}");
 
+        if (!file_exists($fileName)) {
+            $this->makeDir($folderPath);
+
+            $serviceTemplate = file_get_contents(resource_path("stubs/Model.stub"));
+            $serviceTemplate = str_replace(['{{Name}}'], [$name], $serviceTemplate);
+            file_put_contents($fileName, $serviceTemplate);
+
+            return true;
+        } else {
+            echo "WARNING : Services is existed\n";
+
+            return false;
+        }
+    }
+
+    protected function makeDir($folderPath)
+    {
+        if (!file_exists($folderPath)) {
+            mkdir($folderPath, 0777, true);
+        }
     }
 
     /**
